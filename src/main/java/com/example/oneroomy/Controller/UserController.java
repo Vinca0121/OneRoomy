d985f37ba074baa193f19c5aa41c2b941d096796
Vinca0121 <qqw7820@gmail.com>
2023-04-12 16:49:43 +0900

    init project

--- src/main/java/com/example/oneroomy/Controller/UserController.java
+++ src/main/java/com/example/oneroomy/Controller/UserController.java
@@ -1,5 +1,13 @@
 package com.example.oneroomy.Controller;
 
+
+import com.example.oneroomy.Service.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/auth")
 public class UserController {
+
+
+    private UserService userService;
 
 }
---
+++ src/main/java/com/example/oneroomy/Service/UserService.java
@@ -0,0 +1,4 @@
+package com.example.oneroomy.Service;
+
+public class UserService {
+}
