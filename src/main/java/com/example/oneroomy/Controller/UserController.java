12438cd814209f105a06bd31c7ca3e220490f8c9
Vinca0121 <qqw7820@gmail.com>
2023-04-12 15:32:34 +0900

    init project ❤ Can Win

---
+++ src/main/java/com/example/oneroomy/Controller/UserController.java
@@ -0,0 +1,5 @@
+package com.example.oneroomy.Controller;
+
+public class UserController {
+
+}
---
+++ src/test/java/com/example/oneroomy/OneRoomyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oneroomy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneRoomyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
